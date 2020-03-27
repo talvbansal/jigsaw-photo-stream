0e16d6bf026746dc8981c50297449a8b74000a61
Talv Bansal <talvbansal@outlook.com>
2020-03-27 12:07:11 +0000

    Fixed bad repo link

--- source/_partials/social-links.blade.php
+++ source/_partials/social-links.blade.php
@@ -1,5 +1,5 @@
 <ul class="fixed z-10 bottom-0 right-0 flex flex-col sm:flex-row flex-wrap mb-3 mr-3 opacity-75">
-    <li class="cursor-pointer text-xs mr-1 mb-1 rounded px-2 py-1 bg-gray-100 text-gray-800 uppercase hover:bg-white hover:text-gray-900"><a rel="me" href="https://github.com/talvbansal/photo-stream" title="Fork on Github">Fork on Github</a></li>
+    <li class="cursor-pointer text-xs mr-1 mb-1 rounded px-2 py-1 bg-gray-100 text-gray-800 uppercase hover:bg-white hover:text-gray-900"><a rel="me" href="https://github.com/talvbansal/jigsaw-photo-stream" title="Fork on Github">Fork on Github</a></li>
     @if($page->links->twitter)
         <li class="cursor-pointer text-xs mr-1 mb-1 rounded px-2 py-1 bg-gray-100 text-gray-800 uppercase hover:bg-white hover:text-gray-900"><a rel="me" href="https://twitter.com/{{ $page->links->twitter }}" title="{{ '@'.$page->links->twitter }} on Twitter">Twitter</a></li>
     @endif
