7b3bea412db8c1c219c1bcbd2b7f6007c8cfab3b
Talv Bansal <talvbansal@outlook.com>
2020-04-09 10:56:43 +0100

    Add myanmar photos and clean up code display code slightly

--- source/_partials/photo.blade.php
+++ source/_partials/photo.blade.php
@@ -1,21 +1,18 @@
-<div
-     class="open flex-grow relative border-2 vh-60 sm:vh-40 md:vh-30"
-     style="
-background-color: {{ $photo->tint }};
-width:'{{ $photo->width }}px';
-height:'{{ $photo->height }}px';"
-title="{{ $photo->name }}
-">
-    <a href="/photos/{{ $photo->filename }}" @click.prevent="showPhoto('{{ $photo->id }}')">
-        <noscript class="loading-lazy">
-            <img
-                class="inline-block w-auto min-w-full min-h-full max-h-full object-cover
-                transition duration-500 ease-in-out hover:opacity-50"
-                loading="lazy"
-                src="/{{ $photo->thumbnail }}"
-                width="{{ $photo->width }}"
-                height="{{ $photo->height }}"
-            />
-        </noscript>
-    </a>
-</div>
+<a
+    href="/photos/{{ $photo->filename }}"
+    class="open flex-grow relative border-2 vh-50 sm:vh-40 md:vh-30"
+    title="{{ $photo->name }}"
+    @click.prevent="showPhoto('{{ $photo->id }}')"
+>
+    <noscript class="loading-lazy">
+        <img
+            class="inline-block w-auto min-w-full min-h-full max-h-full object-cover
+            transition duration-500 ease-in-out hover:opacity-50"
+            loading="lazy"
+            style="background-color: {{ $photo->tint }}"
+            src="/{{ $photo->thumbnail }}"
+            width="{{ $photo->width }}px"
+            height="{{ $photo->height }}px"
+        />
+    </noscript>
+</a>
