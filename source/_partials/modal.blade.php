b336e273574bc94e357137749ac061b3cd590ab4
Talv Bansal <talvbansal@outlook.com>
2020-04-09 11:11:07 +0100

    Add More photos

--- source/_partials/modal.blade.php
+++ source/_partials/modal.blade.php
@@ -1,9 +1,9 @@
 <div x-show="open" class="close fixed bottom-0 inset-x-0 inset-0 p-0 flex items-center justify-center z-20">
     <div x-show="open"
-         x-transition:enter="ease-out duration-300"
+         x-transition:enter="ease-out duration-500"
          x-transition:enter-start="opacity-0"
          x-transition:enter-end="opacity-100"
-         x-transition:leave="ease-in duration-200"
+         x-transition:leave="ease-in duration-500"
          x-transition:leave-start="opacity-100"
          x-transition:leave-end="opacity-0" class="fixed inset-0 transition-opacity">
         <div class="absolute inset-0 opacity-75 transition ease-in-out duration-500" :style="tint"></div>
@@ -14,10 +14,10 @@
          @keydown.escape="close()"
          @keydown.arrow-left="previousImage(event)"
          @keydown.arrow-right="nextImage(event)"
-         x-transition:enter="ease-out duration-300"
+         x-transition:enter="ease-out duration-500"
          x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
          x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-         x-transition:leave="ease-in duration-200"
+         x-transition:leave="ease-in duration-500"
          x-transition:leave-start="opacity-75 translate-y-0 sm:scale-100"
          x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
          class="flex items-center fixed top-0 right-0 left-0 bottom-0 h-full overflow-hidden shadow-xl transform transition-all duration-1000 ease-in-out z-30"
